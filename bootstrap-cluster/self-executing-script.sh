58315fc44d5c12eba612a10047269b2e9f39cd98
Riley Seaburg <riley@rileyseaburg.com>
2023-10-12 09:23:15 -0400

    Update self-executing-script.sh
    fix execution errors

--- bootstrap-cluster/self-executing-script.sh
+++ bootstrap-cluster/self-executing-script.sh
@@ -1,1 +1,26 @@
-echo -e '#!/bin/bash\nif [[ $EUID -ne 0 ]]; then\n   echo "This script must be run as root" \n   exit 1\nfi\nif ! [ -x "$(command -v kubeadm)" ] || ! [ -x "$(command -v kubelet)" ] || ! [ -x "$(command -v kubectl)" ]; then\n  echo "Error: kubeadm, kubelet or kubectl are not installed." >&2\n  exit 1\nfi\nPOD_NETWORK_CIDR="10.244.0.0/16"\n\n# Initializing Master Node\nkubeadm init --pod-network-cidr=$POD_NETWORK_CIDR\n\n# To start using your cluster, you need to run the following as a regular user\nmkdir -p $HOME/.kube\nsudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config\nsudo chown $(id -u):$(id -g) $HOME/.kube/config\n\n# Installing a Pod Network (We\'ll use Calico as an example)\nkubectl --kubeconfig /etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml\nkubectl --kubeconfig /etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/manifests/custom-resources.yaml\n\necho "Kubernetes Master setup is complete!"\necho "To join a node to this cluster, run the following on the node:"\nkubeadm token create --print-join-command' > setup_k8s_cluster.sh && bash setup_k8s_cluster.sh
+echo -e '#!/bin/bash\n\
+if [[ $EUID -ne 0 ]]; then\n\
+   echo "This script must be run as root" \n\
+   exit 1\n\
+fi\n\
+if ! [ -x "$(command -v kubeadm)" ] || ! [ -x "$(command -v kubelet)" ] || ! [ -x "$(command -v kubectl)" ]; then\n\
+  echo "Error: kubeadm, kubelet or kubectl are not installed." >&2\n\
+  exit 1\n\
+fi\n\
+POD_NETWORK_CIDR="10.244.0.0/16"\n\
+\n\
+# Initializing Master Node\n\
+kubeadm init --pod-network-cidr=$POD_NETWORK_CIDR\n\
+\n\
+# To start using your cluster, you need to run the following as a regular user\n\
+mkdir -p $HOME/.kube\n\
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config\n\
+sudo chown $(id -u):$(id -g) $HOME/.kube/config\n\
+\n\
+# Installing a Pod Network (We'\''ll use Calico as an example)\n\
+kubectl --kubeconfig /etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml\n\
+kubectl --kubeconfig /etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/manifests/custom-resources.yaml\n\
+\n\
+echo "Kubernetes Master setup is complete!"\n\
+echo "To join a node to this cluster, run the following on the node:"\n\
+kubeadm token create --print-join-command' > setup_k8s_cluster.sh && bash setup_k8s_cluster.sh
