984fd7dfd56f9e6fd1ca41135957420004ed25e8
Riley Seaburg <riley@rileyseaburg.com>
2023-10-14 11:41:47 -0400

    Update self-executing-script.sh
    feat: Enhance Kubernetes Setup Script
    
    - Add IP forwarding enablement to support network packet routing between nodes.
    - Ensure swap is disabled to comply with K8s production prerequisites.
    - Adjust the Docker setup and streamline package installation for kubeadm, kubelet, and kubectl.
    
    This enhancement ensures the setup script adheres to Kubernetes best practices and prerequisites for setup on nodes. By ensuring IP forwarding is enabled and swap is disabled, we align the environment with the necessary conditions for stable Kubernetes operation.

--- worker-nodes/self-executing-script.sh
+++ worker-nodes/self-executing-script.sh
@@ -1,10 +1,8 @@
 echo '#!/bin/bash -e
 
-# Update package information
+# Update package information and install necessary packages
 sudo apt-get update
-
-# Install Docker
-sudo apt-get install -y docker.io
+sudo apt-get install -y docker.io apt-transport-https curl
 
 # Setup Docker daemon
 cat <<EOF | sudo tee /etc/docker/daemon.json
@@ -18,27 +16,29 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
 }
 EOF
 
+# Enable and restart Docker service
 sudo systemctl enable docker
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
-# Install kubeadm, kubelet and kubectl
-sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+# Add Kubernetes repository
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
+# Install kubeadm, kubelet and kubectl
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
-# Disable swap (Assuming that swap is enabled)
+# Disable swap and comment it out in /etc/fstab for persistence
 sudo swapoff -a
-
-# Comment swap entry in /etc/fstab to make the swapoff persistent across reboots
 sudo sed -i '\''/ swap / s/^\(.*\)$/#\1/g'\'' /etc/fstab
+
+# Enable IP forwarding
+echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
 
 echo "Installation of Docker and Kubeadm is completed"
 ' > install-kube_admin.sh && chmod +x install-kube_admin.sh && sudo bash install-kube_admin.sh
