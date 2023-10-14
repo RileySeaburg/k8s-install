f67de1e4b357451004896c62597459f712854f90
Riley Seaburg <riley@rileyseaburg.com>
2023-10-14 11:47:05 -0400

    Update self-executing-script.sh
    feat: update setup script to use containerd
    
    - Modified the script to install and configure containerd instead of Docker
    - Adjusted kubelet configuration to use containerd as the CRI (Container Runtime Interface)
    - Ensured that swap is disabled and IP forwarding is enabled as per Kubernetes prerequisites
    
    This update ensures that nodes set up using the script use containerd directly,
    optimizing resource usage and aligning with best practices for Kubernetes setup.

--- worker-nodes/self-executing-script.sh
+++ worker-nodes/self-executing-script.sh
@@ -2,24 +2,17 @@ echo '#!/bin/bash -e
 
 # Update package information and install necessary packages
 sudo apt-get update
-sudo apt-get install -y docker.io apt-transport-https curl
+sudo apt-get install -y containerd apt-transport-https curl
 
-# Setup Docker daemon
-cat <<EOF | sudo tee /etc/docker/daemon.json
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
+# Configure containerd
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
 
-# Enable and restart Docker service
-sudo systemctl enable docker
-sudo systemctl daemon-reload
-sudo systemctl restart docker
+# (Optional) Additional configurations for containerd can be added here...
+
+# Restart containerd to apply the configuration
+sudo systemctl restart containerd
+sudo systemctl enable containerd
 
 # Add Kubernetes repository
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
@@ -27,10 +20,15 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
-# Install kubeadm, kubelet and kubectl
+# Install kubeadm, kubelet, and kubectl
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+
+# Configure kubelet to use containerd
+sudo sed -i "s/cgroup-driver=systemd/cgroup-driver=systemd/g" /var/lib/kubelet/kubeadm-flags.env
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
 
 # Disable swap and comment it out in /etc/fstab for persistence
 sudo swapoff -a
@@ -40,5 +38,5 @@ sudo sed -i '\''/ swap / s/^\(.*\)$/#\1/g'\'' /etc/fstab
 echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
-echo "Installation of Docker and Kubeadm is completed"
+echo "Installation of Containerd and Kubeadm is completed"
 ' > install-kube_admin.sh && chmod +x install-kube_admin.sh && sudo bash install-kube_admin.sh
